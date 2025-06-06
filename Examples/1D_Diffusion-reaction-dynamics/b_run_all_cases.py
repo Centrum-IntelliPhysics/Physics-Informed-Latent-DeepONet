2199ff7c95a3fd371514108f42baeeae5ad36e42
sharmila-k <sharmilak.iitm@gmail.com>
2025-06-06 01:24:19 -0400

    examples

---
+++ Examples/1D_Diffusion-reaction-dynamics/b_run_all_cases.py
@@ -0,0 +1,22 @@
+import os
+
+# Define the values
+seed_values = [0, 1, 2, 3, 4]
+n_used_values = [0, 100, 200]
+
+# Iterate over each combination
+for seed in seed_values:
+    for n_used in n_used_values:
+        # Construct the directory path
+        resultdir = os.path.join(os.getcwd(), 'results', 'b_Latent-NO', f'seed={seed}_n_used={n_used}')
+        if not os.path.exists(resultdir):
+            os.makedirs(resultdir)
+
+        # Set save to True only for seed 0
+        save = True if seed == 0 else False
+        
+        # Construct the command to submit the Slurm job with the current values of a and b
+        command = f"sbatch --export=seed={seed},n_used={n_used},save={save} b_job_ROCKFISH_gpu.sh"
+        
+        # Execute the command
+        os.system(command)
---
+++ Examples/2D_Burgers_transport_dynamics/b_job_ROCKFISH_gpu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+#SBATCH
+#SBATCH --job-name=check
+#SBATCH --time=24:00:00
+#SBATCH --partition=a100
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH -A sgoswam4_gpu
+#SBATCH --mail-user=skarumu1@jh.edu
+#SBATCH --mail-type=ALL
+
+
+ml anaconda
+conda activate py311
+
+
+# Get parameters from the environment variables
+seed=$seed
+n_used=$n_used
+save=$save
+
+# Construct the output directory path
+resultdir="results/b_Latent-NO/seed=${seed}_n_used=${n_used}"
+mkdir -p $resultdir
+
+# Run the papermill command with the passed parameters and save the output notebook in the constructed directory
+papermill b_Latent-NO.ipynb "${resultdir}/output_seed=${seed}_n_used=${n_used}.ipynb" -p seed $seed -p n_used $n_used -p save $save > /dev/null 2>&1
